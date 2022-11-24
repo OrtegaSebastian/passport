1eb0982601ef87bd2d26ef238aa53efc6b604cf9
sebaortega <ortegamolarosebastian@gmail.com>
2022-11-24 19:50:48 -0300

    AgregoCambios

--- routers/web/auth.js
+++ routers/web/auth.js
@@ -1,19 +1,89 @@
 import { Router } from 'express'
-
+import DAOUsuarios from "../../daos/UsuariosDAO.js";
+import url from 'url'
+import { join } from "path";
+import { Strategy } from "passport-local";
+import bcrypt from "bcrypt";
+import passport from "passport";
 import path from 'path'
+
+const MongoUsers = new DAOUsuarios();
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url))
-const rutaLogin = join(__dirname,"public/login.html")
-const rutaBienvenido = join(__dirname,"public/bienvenido.html")
-const rutaRegistro = join(__dirname,"public/registro.html")
-const rutaError = join(__dirname,"public/error.html")
-
-
+const rutaLogin = join(__dirname,"../../public/login.html")
+const rutaBienvenido = join(__dirname,"../../public/bienvenido.html")
+const rutaRegistro = join(__dirname,"../../public/registro.html")
+const rutaError = join(__dirname,"../../public/error.html")
 
 const authWebRouter = new Router()
 
-authWebRouter.get('/', (req, res) => {
-    res.redirect('/home')
-})
+
+//passport
+passport.use(
+    "registro",
+new Strategy(
+    {
+    passReqToCallback: true,
+    },
+    (req, username, password, done) => {
+    const { email } = req.body;
+    const user = usuarios.find((u) => u.username == username);
+
+    if (user) return done("Usuario registrado");
+
+    usuarios.push({ username, password, email });
+    const userObj = { username, password, email };
+    return done(null, userObj);
+    }
+)
+);
+
+
+passport.use(
+    "login",
+new Strategy(
+    { passReqToCallback: true },
+    (req, username, password, done) => {
+    const user = usuarios.find(
+        (user) => user.username == username && user.password == password
+    );
+    if (!user) return done("usuario no existe");
+    return done(null, user);
+    }
+)
+);
+passport.serializeUser((user, done) => {
+done(null, user.username);
+});
+
+passport.deserializeUser((username, done) => {
+const user = usuarios.find((user) => user.username == username);
+
+done(null, user);
+});
+
+const hasPassword = (pass) => {
+// ocultar
+return bcrypt.hashSync(pass, bcrypt.genSaltSync(10), null);
+};
+const validatePass = (pass, hashedPass) => {
+// validar
+return bcrypt.compareSync(pass, hashedPass);
+};
+
+
+
+//routers
+authWebRouter.get('/', (req, res) => {   
+    if (req.query.username) req.session.usuario = req.query.username;
+    if (req.session.usuario) {
+    res.sendFile(rutaBienvenido)
+    res.send(
+        `Hola ${req.session.usuario} `
+    );
+    }else {
+        res.sendFile(rutaLogin);
+}})
+
 
 authWebRouter.get('/registro',(req,res)=>{      
     if (req.session.usuario) {
@@ -27,7 +97,28 @@ authWebRouter.get('/registro',(req,res)=>{
     }}
     )
 
-authWebRouter.get('/login', (req, res) => {
+authWebRouter.post('/registro', async(req,res)=>{    
+    const { username, password } = req.body;
+    const result = await MongoUsers.guardar({ username, password });
+    req.session.usuario = username;
+    res.redirect("/");
+    res.send(`'usuario registrado' ${result}`)
+})
+
+authWebRouter.post('/',async(req,res)=>{
+    try {
+        const { username, password } = req.body;
+        const usuario = await MongoUsers.listar(username, password);
+        console.log(usuario);
+        req.session.usuario = username;
+        res.redirect("/");
+    } catch (e) {
+        res.redirect("/?error=true");
+    }
+})
+
+
+authWebRouter.get('/login',hasPassword, (req, res) => {
     const username = req.session?.username
     if (username) {
         res.redirect('/')
@@ -35,6 +126,8 @@ authWebRouter.get('/login', (req, res) => {
         res.sendFile(path.join(process.cwd(), '/public/login.html'))
     }
 })
+
+
 
 authWebRouter.get('/logout', (req, res) => {
     const username = req.session?.username
@@ -52,7 +145,7 @@ authWebRouter.get('/logout', (req, res) => {
 })
 
 
-authWebRouter.post('/login', (req, res) => {
+authWebRouter.post('/login', validatePass, (req, res) => {
     req.session.username = req.body.username
     res.redirect('/home')
 })
--- server.js
+++ server.js
@@ -3,19 +3,10 @@ import session from "express-session";
 import bodyParser from "body-parser";
 import MongoStore from "connect-mongo";
 import { Server as HttpServer } from 'http'
-// import DAOUsuarios from "./daos/UsuariosDAO.js";
-// import url from 'url'
-// import { join } from "path";
-// import { Strategy } from "passport-local";
-// import bcrypt from "bcrypt";
-// import passport from "passport";
 import authWebRouter from './routers/web/auth.js'
 import homeWebRouter from './routers/web/home.js'
 const app = express();
 const httpServer = new HttpServer(app)
-// const MongoUsers = new DAOUsuarios();
-
-
 
 const mongoOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 
@@ -23,11 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'))
-// const __dirname = url.fileURLToPath(new URL(".", import.meta.url))
-// const rutaLogin = join(__dirname,"public/login.html")
-// const rutaBienvenido = join(__dirname,"public/bienvenido.html")
-// const rutaRegistro = join(__dirname,"public/registro.html")
-// const rutaError = join(__dirname,"public/error.html")
+
 
 app.use(session({
     secret:'12354asd223bbthhhasd',
@@ -64,56 +51,10 @@ connectedServer.on('error', error => console.log(`Error en servidor ${error}`))
 
 
 
-
-
-
-
-
 // signup
 
 // //registro
-// passport.use(
-//     "registro",
-// new localStrategy(
-//     {
-//     passReqToCallback: true,
-//     },
-//     (req, username, password, done) => {
-//     const { email } = req.body;
-//     const user = usuarios.find((u) => u.username == username);
 
-//     if (user) return done("Usuario registrado");
-
-//     usuarios.push({ username, password, email });
-//     const userObj = { username, password, email };
-//     return done(null, userObj);
-//     }
-// )
-// );
-
-
-// passport.use(
-//     "login",
-// new localStrategy(
-//     { passReqToCallback: true },
-//     (req, username, password, done) => {
-//     const user = usuarios.find(
-//         (user) => user.username == username && user.password == password
-//     );
-//     if (!user) return done("usuario no existe");
-//     return done(null, user);
-//     }
-// )
-// );
-// passport.serializeUser((user, done) => {
-// done(null, user.username);
-// });
-
-// passport.deserializeUser((username, done) => {
-// const user = usuarios.find((user) => user.username == username);
-
-// done(null, user);
-// });
 
 // const hasPassword = (pass) => {
 // // ocultar
@@ -127,63 +68,6 @@ connectedServer.on('error', error => console.log(`Error en servidor ${error}`))
 // const auth= (req,res,next)=>{
 //     req.session.isAdmin == true?next():res.status(401).send('sin permisos')
 // }
-
-// app.get('/',(req,res)=>{   
-//     if (req.query.username) req.session.usuario = req.query.username;
-//     if (req.session.usuario) {
-//     res.sendFile(rutaBienvenido)
-//     res.send(
-//         `Hola ${req.session.usuario} `
-//     );
-//     }else {
-//         res.sendFile(rutaLogin);
-//     }
-//     }
-// )
-// app.get('/registro',(req,res)=>{      
-// if (req.session.usuario) {
-// res.sendFile(rutaBienvenido);
-// } else {
-// if (req.query.error) {
-//     res.sendFile(rutaError);
-// } else {
-//     res.sendFile(rutaLogin);
-// }
-// }}
-// )
-
-// app.post('/',async(req,res)=>{
-//     try {
-//         const { username, password } = req.body;
-//         const usuario = await MongoUsers.listar(username, password);
-//         console.log(usuario);
-//         req.session.usuario = username;
-//         res.redirect("/");
-//     } catch (e) {
-//         res.redirect("/?error=true");
-//     }
-// })
-
-// app.post('/registro', async(req,res)=>{    
-//     const { username, password } = req.body;
-//     const result = await MongoUsers.guardar({ username, password });
-//     req.session.usuario = username;
-//     res.redirect("/");
-//     res.send(`'usuario registrado' ${result}`)
-// })
-
-// app.get('/logout',(req,res)=>{
-//     req.session.destroy((err) => {
-//         res.redirect("/");
-//     });
-// })
-
-
-
-// app.listen(8082, () => console.log("conectados"));
-
-
-
 
 
 
